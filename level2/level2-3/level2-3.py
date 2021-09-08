bc80ee93a25a544cb2384bac3cfa05ecd4d791c6
KOOKDONGHUN <dh3978@naver.com>
2021-09-08 13:59:56 +0900

    add comment

--- level2/level2-3/level2-3.py
+++ level2/level2-3/level2-3.py
@@ -36,6 +36,25 @@ def solution(record):
 #     user_id = {EC.split()[1]:EC.split()[-1] for EC in record if EC.startswith('Enter') or EC.startswith('Change')}
 #     return [f'{user_id[E_L.split()[1]]}님이 들어왔습니다.' if E_L.startswith('Enter') else f'{user_id[E_L.split()[1]]}님이 나갔습니다.' for E_L in record if not E_L.startswith('Change')]
 
+# def solution(record):
+#     answer = []
+#     log_list = []
+#     log_dict = dict()
+#
+#     for one_log in record:
+#         log = one_log.split(" ")
+#
+#         if log[0] == "Enter":
+#             log_list.append([log[1],"님이 들어왔습니다."])
+#             log_dict[log[1]] = log[2]
+#         elif log[0] == "Leave":
+#             log_list.append([log[1],"님이 나갔습니다."])
+#         else:
+#             log_dict[log[1]] = log[2]
+#
+#     for i in range(len(log_list)):
+#         answer.append(log_dict[log_list[1][0]] + log_list[1][1])
+#     return answer
 
 if __name__ == "__main__":
     # execute only if run as a script
